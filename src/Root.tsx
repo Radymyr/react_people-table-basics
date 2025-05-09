c224ad57a6e605abaffd24f663dde751d18f20f9
Radymyr <loboda.radymyr@gmail.com>
2025-05-09 17:27:54 +0300

    fixed

--- src/Root.tsx
+++ src/Root.tsx
@@ -12,19 +12,24 @@ import { NotFoundPage } from './pages/notFoundPage/notFoundPage';
 import { RedirectToPerson } from './components/redirectToSlug/redirectToSlug';
 
 export const Root = () => {
+  const people = '/people';
+  const person = '/person';
+  const rootPath = '/';
+  const home = '/home';
+
   return (
     <Router>
       <PeopleProvider>
         <Routes>
-          <Route path="/" element={<App />}>
+          <Route path={rootPath} element={<App />}>
             <Route index element={<HomePage />} />
-            <Route path="people" element={<Navigate to="/person" replace />} />
-            <Route path="/people/:slug" element={<RedirectToPerson />} />
-            <Route path="person" element={<PeoplePage />}>
+            <Route path={people} element={<Navigate to={person} replace />} />
+            <Route path={`${people}/:slug`} element={<RedirectToPerson />} />
+            <Route path={person} element={<PeoplePage />}>
               <Route path=":slug" element={<PeoplePage />} />
             </Route>
             <Route path="*" element={<NotFoundPage />} />
-            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path={home} element={<Navigate to={rootPath} replace />} />
           </Route>
         </Routes>
       </PeopleProvider>
